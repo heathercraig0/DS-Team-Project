c9034a93a0281b0eba5811d4c8e86653513eb62e
heathercraig0 <hk.craig@mail.utoronto.ca>
2026-07-22 11:40:03 -0400

    Initial R Code
    seeing if we can make GitHub work lol

---
+++ Team6_1859_Project.R
@@ -0,0 +1,22 @@
+#Data Science 1859 Team Project
+#Team 6 - Heather Craig, Amanda Illeperuma, Edward Li & Jayati Mishra
+
+# Reading in data and basic info
+given_data <- read.csv("project_data.csv")
+head(given_data)
+summary(given_data)
+dim(given_data)
+colnames(given_data)
+
+# Making new DF with only variables of interest
+key_variables<- data[c("Pittsburgh.Sleep.Quality.Index.Score","Epworth.Sleepiness.Scale","Stanford.Sleepiness.Scale","Athens.Insomnia.Scale","SF36.PCS","SF36.MCS","Age","Gender","BMI","Time.from.transplant","Liver.Diagnosis","Recurrence.of.disease","Rejection.graft.dysfunction","Any.fibrosis","Renal.Failure","Depression","Corticoid")]
+
+# Renaming column names
+colnames(key_variables) <- c("PSQI", "Epworth", "Stanford", "AIS", "SF36_PCS", "SF36_MCS"," Age", "Gender", "BMI", "TransplantTime", "LiverDiagnosis", "DiseaseRecurrence",  "Rejection", "Fibrosis", "RenalFailure", "Depression", "Corticosteroid")
+
+# Chekcing missing data
+colSums(is.na(key_variables))
+
+# Checking levels for categorical variables with <2 levels
+
+
