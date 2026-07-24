5c8dcf6f6e58f9c2a47e3cceae486f95383d4f83
starguardiandk <edwardli0704@gmail.com>
2026-07-23 20:01:26 -0400

    key variables csv file

--- Team6_1859_Project.R
+++ Team6_1859_Project.R
@@ -12,7 +12,7 @@ colnames(given_data)
 key_variables<- given_data[c("Pittsburgh.Sleep.Quality.Index.Score","Epworth.Sleepiness.Scale", "Berlin.Sleepiness.Scale","Athens.Insomnia.Scale","SF36.PCS","SF36.MCS","Age","Gender","BMI","Time.from.transplant","Liver.Diagnosis","Recurrence.of.disease","Rejection.graft.dysfunction","Any.fibrosis","Renal.Failure","Depression","Corticoid")]
 
 # Renaming column names
-colnames(key_variables) <- c("PSQI", "ESS", "BSS", "AIS", "SF36_PCS", "SF36_MCS"," Age", "Gender", "BMI", "TransplantTime", "LiverDiagnosis", "DiseaseRecurrence",  "Rejection", "Fibrosis", "RenalFailure", "Depression", "Corticosteroid")
+colnames(key_variables) <- c("PSQI", "ESS", "BSS", "AIS", "SF36_PCS", "SF36_MCS","Age", "Gender", "BMI", "TransplantTime", "LiverDiagnosis", "DiseaseRecurrence",  "Rejection", "Fibrosis", "RenalFailure", "Depression", "Corticosteroid")
 
 
 # Derived Variables
@@ -23,6 +23,9 @@ colnames(key_variables) <- c("PSQI", "ESS", "BSS", "AIS", "SF36_PCS", "SF36_MCS"
 colSums(is.na(key_variables))
 
 
+# attach the Subject ID from raw data
+key_variables$Subject <- given_data$Subject
+key_variables <- key_variables[c("Subject", setdiff(names(key_variables), "Subject"))]
 
 
 write.csv(key_variables, "key_variables.csv", row.names = FALSE)
